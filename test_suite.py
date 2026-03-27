da02ca12ef8a966c39b5f8c38a8cc1de0fcf733d
Nim <nim.steen@calco.nl>
2026-03-27 11:06:01 +0100

    Update test_suite.py
    rename the default parts for tests and place them where each test can access them

--- test_suite.py
+++ test_suite.py
@@ -1,76 +1,77 @@
 import aeroparts_order_app as app
 from datetime import datetime
 
+# insert str: app.Part dictionary with some possible parts, where the str is the part_no
+# each part named for the type of item: A/B (plane), C/NC (certificate or no certificate), SL/NSL (shelf life or no shelf life)
+parts = {
+  "A-C-SL": app.Part(
+    part_no = "A-C-SL",
+    description= "Plane A with certificate and shelf life", # irrelevant in script
+    aircraft_type = "A320",           # e.g. "A320", "B737" 
+    requires_certificate = True,   # EASA Form 1 / FAA 8130-3 
+    shelf_life_days = 10,  # None means no shelf life 
+    hazmat = False, # irrelevant in script
+  ),
+  "A-C-NSL": app.Part(
+    part_no = "A-C-NSL",
+    description= "Plane A with certificate and no shelf life", # irrelevant in script
+    aircraft_type = "A320",           # e.g. "A320", "B737" 
+    requires_certificate = True,   # EASA Form 1 / FAA 8130-3 
+    shelf_life_days = None,  # None means no shelf life 
+    hazmat = False, # irrelevant in script
+  ),
+  "A-NC-SL": app.Part(
+    part_no = "A-NC-SL",
+    description= "Plane A with shelf life and no certificate", # irrelevant in script
+    aircraft_type = "A320",           # e.g. "A320", "B737" 
+    requires_certificate = False,   # EASA Form 1 / FAA 8130-3 
+    shelf_life_days = 10,  # None means no shelf life 
+    hazmat = False, # irrelevant in script
+  ),
+  "A-NC-NSL": app.Part(
+    part_no = "4",
+    description= "Plane A with no certificate and shelf life", # irrelevant in script
+    aircraft_type = "A320",           # e.g. "A320", "B737" 
+    requires_certificate = False,   # EASA Form 1 / FAA 8130-3 
+    shelf_life_days = None,  # None means no shelf life 
+    hazmat = False, # irrelevant in script
+  ),
+  "B-C-SL": app.Part(
+    part_no = "B-C-SL",
+    description= "Plane B with certificate and shelf life", # irrelevant in script
+    aircraft_type = "B737",           # e.g. "A320", "B737" 
+    requires_certificate = True,   # EASA Form 1 / FAA 8130-3 
+    shelf_life_days = 10,  # None means no shelf life 
+    hazmat = False, # irrelevant in script
+  ),
+  "B-C-NSL": app.Part(
+    part_no = "B-C-NSL",
+    description= "Plane B with certificate and no shelf life", # irrelevant in script
+    aircraft_type = "B737",           # e.g. "A320", "B737" 
+    requires_certificate = True,   # EASA Form 1 / FAA 8130-3 
+    shelf_life_days = None,  # None means no shelf life 
+    hazmat = False, # irrelevant in script
+  ),
+  "B-NC-SL": app.Part(
+    part_no = "B-NC-SL",
+    description= "Plane B with shelf life and no certificate", # irrelevant in script
+    aircraft_type = "B737",           # e.g. "A320", "B737" 
+    requires_certificate = False,   # EASA Form 1 / FAA 8130-3 
+    shelf_life_days = 10,  # None means no shelf life 
+    hazmat = False, # irrelevant in script
+  ),
+  "B-NC-NSL": app.Part(
+    part_no = "B-NC-NSL",
+    description= "Plane B with no certificate and shelf life", # irrelevant in script
+    aircraft_type = "B737",           # e.g. "A320", "B737" 
+    requires_certificate = False,   # EASA Form 1 / FAA 8130-3 
+    shelf_life_days = None,  # None means no shelf life 
+    hazmat = False, # irrelevant in script
+  ),
+} 
+
 # A class to test the full system, so everything that happens when place_order is called
 class TestClassSystem:
-  # insert str: app.Part dictionary with some possible parts, where the str is the part_no
-  parts = {
-    "1": app.Part(
-      part_no = "1",
-      description= "Plane A with certificate and shelf life", # irrelevant in script
-      aircraft_type = "A320",           # e.g. "A320", "B737" 
-      requires_certificate = True,   # EASA Form 1 / FAA 8130-3 
-      shelf_life_days = 10,  # None means no shelf life 
-      hazmat = False, # irrelevant in script
-    ),
-    "2": app.Part(
-      part_no = "2",
-      description= "Plane A with certificate and no shelf life", # irrelevant in script
-      aircraft_type = "A320",           # e.g. "A320", "B737" 
-      requires_certificate = True,   # EASA Form 1 / FAA 8130-3 
-      shelf_life_days = None,  # None means no shelf life 
-      hazmat = False, # irrelevant in script
-    ),
-    "3": app.Part(
-      part_no = "3",
-      description= "Plane A with shelf life and no certificate", # irrelevant in script
-      aircraft_type = "A320",           # e.g. "A320", "B737" 
-      requires_certificate = False,   # EASA Form 1 / FAA 8130-3 
-      shelf_life_days = 10,  # None means no shelf life 
-      hazmat = False, # irrelevant in script
-    ),
-    "4": app.Part(
-      part_no = "4",
-      description= "Plane A with no certificate and shelf life", # irrelevant in script
-      aircraft_type = "A320",           # e.g. "A320", "B737" 
-      requires_certificate = False,   # EASA Form 1 / FAA 8130-3 
-      shelf_life_days = None,  # None means no shelf life 
-      hazmat = False, # irrelevant in script
-    ),
-    "5": app.Part(
-      part_no = "5",
-      description= "Plane B with certificate and shelf life", # irrelevant in script
-      aircraft_type = "B737",           # e.g. "A320", "B737" 
-      requires_certificate = True,   # EASA Form 1 / FAA 8130-3 
-      shelf_life_days = 10,  # None means no shelf life 
-      hazmat = False, # irrelevant in script
-    ),
-    "6": app.Part(
-      part_no = "6",
-      description= "Plane B with certificate and no shelf life", # irrelevant in script
-      aircraft_type = "B737",           # e.g. "A320", "B737" 
-      requires_certificate = True,   # EASA Form 1 / FAA 8130-3 
-      shelf_life_days = None,  # None means no shelf life 
-      hazmat = False, # irrelevant in script
-    ),
-    "7": app.Part(
-      part_no = "7",
-      description= "Plane B with shelf life and no certificate", # irrelevant in script
-      aircraft_type = "B737",           # e.g. "A320", "B737" 
-      requires_certificate = False,   # EASA Form 1 / FAA 8130-3 
-      shelf_life_days = 10,  # None means no shelf life 
-      hazmat = False, # irrelevant in script
-    ),
-    "8": app.Part(
-      part_no = "8",
-      description= "Plane B with no certificate and shelf life", # irrelevant in script
-      aircraft_type = "B737",           # e.g. "A320", "B737" 
-      requires_certificate = False,   # EASA Form 1 / FAA 8130-3 
-      shelf_life_days = None,  # None means no shelf life 
-      hazmat = False, # irrelevant in script
-    ),
-  } 
-  
   stock = [] # insert List of app.StockItem with the items available in warehouses
   offers = [] # insert List of app.SupplierOffer with the items that can be bought from suppliers
 
@@ -81,7 +82,7 @@ class TestClassSystem:
     # insert data in request for the tested order request
     # optionally change the self.parts/self.stock/self.offers for the specific test
 
-    response = app.place_order(request, self.parts, self.stock, self.offers)
+    response = app.place_order(request, parts, self.stock, self.offers)
     
     assert "statement to test (probably from response?)" == "whatever you want to test"
     # test will pass if the 'assert' statement is correct, and fail otherwise
@@ -92,80 +93,14 @@ class TestClassSystem:
 
 # A class to test the method validate_request
 class TestClassUnitValidateRequest:
-  # insert str: app.Part dictionary with some possible parts, where the str is the part_no
-  parts = {
-    "1": app.Part(
-      part_no = "1",
-      description= "Plane A with certificate and shelf life", # irrelevant in script
-      aircraft_type = "A320",           # e.g. "A320", "B737" 
-      requires_certificate = True,   # EASA Form 1 / FAA 8130-3 
-      shelf_life_days = 10,  # None means no shelf life 
-      hazmat = False, # irrelevant in script
-    ),
-    "2": app.Part(
-      part_no = "2",
-      description= "Plane A with certificate and no shelf life", # irrelevant in script
-      aircraft_type = "A320",           # e.g. "A320", "B737" 
-      requires_certificate = True,   # EASA Form 1 / FAA 8130-3 
-      shelf_life_days = None,  # None means no shelf life 
-      hazmat = False, # irrelevant in script
-    ),
-    "3": app.Part(
-      part_no = "3",
-      description= "Plane A with shelf life and no certificate", # irrelevant in script
-      aircraft_type = "A320",           # e.g. "A320", "B737" 
-      requires_certificate = False,   # EASA Form 1 / FAA 8130-3 
-      shelf_life_days = 10,  # None means no shelf life 
-      hazmat = False, # irrelevant in script
-    ),
-    "4": app.Part(
-      part_no = "4",
-      description= "Plane A with no certificate and shelf life", # irrelevant in script
-      aircraft_type = "A320",           # e.g. "A320", "B737" 
-      requires_certificate = False,   # EASA Form 1 / FAA 8130-3 
-      shelf_life_days = None,  # None means no shelf life 
-      hazmat = False, # irrelevant in script
-    ),
-    "5": app.Part(
-      part_no = "5",
-      description= "Plane B with certificate and shelf life", # irrelevant in script
-      aircraft_type = "B737",           # e.g. "A320", "B737" 
-      requires_certificate = True,   # EASA Form 1 / FAA 8130-3 
-      shelf_life_days = 10,  # None means no shelf life 
-      hazmat = False, # irrelevant in script
-    ),
-    "6": app.Part(
-      part_no = "6",
-      description= "Plane B with certificate and no shelf life", # irrelevant in script
-      aircraft_type = "B737",           # e.g. "A320", "B737" 
-      requires_certificate = True,   # EASA Form 1 / FAA 8130-3 
-      shelf_life_days = None,  # None means no shelf life 
-      hazmat = False, # irrelevant in script
-    ),
-    "7": app.Part(
-      part_no = "7",
-      description= "Plane B with shelf life and no certificate", # irrelevant in script
-      aircraft_type = "B737",           # e.g. "A320", "B737" 
-      requires_certificate = False,   # EASA Form 1 / FAA 8130-3 
-      shelf_life_days = 10,  # None means no shelf life 
-      hazmat = False, # irrelevant in script
-    ),
-    "8": app.Part(
-      part_no = "8",
-      description= "Plane B with no certificate and shelf life", # irrelevant in script
-      aircraft_type = "B737",           # e.g. "A320", "B737" 
-      requires_certificate = False,   # EASA Form 1 / FAA 8130-3 
-      shelf_life_days = None,  # None means no shelf life 
-      hazmat = False, # irrelevant in script
-    ),
-  } 
+  pass
 
   def example_test(self):
     request = app.OrderRequest("some id", "some no", "some type", 0, "prio", "requester", datetime.now(), "center")
     # insert data in request for the tested order request
     # optionally change the self.parts for the specific test
 
-    response = app.validate_request(request, self.parts)
+    response = app.validate_request(request, parts)
 
     assert "statement to test (probably from response?)" == "whatever you want to test"
     # test will pass if the 'assert' statement is correct, and fail otherwise
